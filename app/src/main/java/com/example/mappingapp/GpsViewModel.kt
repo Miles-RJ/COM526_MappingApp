3ae0dc1655c5ce56ad1df390fa4d5fa400e1bd9d
Miles Robb-Jones <0robbm88@solent.ac.uk>
2026-02-11 16:34:21 +0000

    Completed week 4 mapping requirements

--- app/src/main/java/com/example/mappingapp/GpsViewModel.kt
+++ app/src/main/java/com/example/mappingapp/GpsViewModel.kt
@@ -2,21 +2,14 @@ package com.example.mappingapp
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import org.maplibre.android.geometry.LatLng
 
 class GpsViewModel : ViewModel() {
-    var latitude: Double = 0.0
-        set(newLat) {
-            field=newLat
-            latLiveData.value = newLat
+    var latLon = LatLng(0.0, 0.0)
+        set(newLatLon) {
+            field = newLatLon
+            latLonLiveData.value = newLatLon
         }
-    var latLiveData = MutableLiveData<Double>()
-
-    var longitude: Double = 0.0
-        set(newLon) {
-            field=newLon
-            lonLiveData.value = newLon
-        }
-    var lonLiveData = MutableLiveData<Double>()
-
+    var latLonLiveData = MutableLiveData<LatLng>()
 
 }
--- app/src/main/java/com/example/mappingapp/MainActivity.kt
+++ app/src/main/java/com/example/mappingapp/MainActivity.kt
@@ -17,13 +17,16 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,99 +34,184 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import androidx.core.text.isDigitsOnly
 import com.example.mappingapp.ui.theme.MappingAppTheme
 import org.maplibre.android.geometry.LatLng
 import org.maplibre.android.maps.Style
 import org.ramani.compose.CameraPosition
+import org.ramani.compose.Circle
 import org.ramani.compose.MapLibre
+import org.ramani.compose.Polygon
+import org.ramani.compose.Polyline
 
 
 class MainActivity : ComponentActivity(), LocationListener {
     val viewModel : GpsViewModel by viewModels()
+    val liveLocation = false
     val styleBuilder = Style.Builder().fromUri("https://tiles.openfreemap.org/styles/bright")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startGPS()
+        viewModel.let {
+            it.latLon = LatLng(50.9079, -1.4015)
+        }
         setContent {
             MappingAppTheme {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth().weight(9f),
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        DisplayMap()
-                    }
-                    Row(
-                        modifier = Modifier.fillMaxWidth().weight(1f),
-                        horizontalArrangement = Arrangement.Center
-                    ) {
+
+                val enterLatLonHeight = 40.dp
+
+
+                    Column(modifier=Modifier.fillMaxSize()) {
+
                         DisplayLatLon()
+
+                        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+
+                            DisplayMap(
+                                Modifier.align(Alignment.TopCenter)
+                                    .height(this.maxHeight - enterLatLonHeight)
+                            )
+
+                            EnterLatLon(
+                                Modifier.height(enterLatLonHeight).align(Alignment.BottomCenter)
+                            )
+                        }
                     }
-                }
+
             }
         }
     }
 
     @Composable
-    fun DisplayMap() {
+    fun DisplayMap(modifier: Modifier = Modifier) {
 
         // Get Lat and on values from view model
-        val lat = remember { mutableStateOf(0.0) }
-        viewModel.latLiveData.observe(this){
-            lat.value = it
-        }
-        val lon = remember { mutableStateOf(0.0) }
-        viewModel.latLiveData.observe(this) {
-            lon.value = it
+        val latLon = remember { mutableStateOf(LatLng(0.0, 0.0)) }
+        viewModel.latLonLiveData.observe(this){
+            latLon.value = it
         }
 
         MapLibre(
-            modifier = Modifier.fillMaxSize().border(BorderStroke(5.dp, Color.Red)),
+            modifier = modifier
+                .fillMaxSize()
+                .border(BorderStroke(2.dp, Color.Red)),
             styleBuilder = styleBuilder,
             cameraPosition = CameraPosition(
-                target = LatLng(lat.value, lon.value),
-                zoom = 14.0
+                target = latLon.value,
+                zoom = 12.0
             )
+        ){
+            ShowCheckpointsOnMap()
+        }
+    }
+
+    @Composable
+    fun EnterLatLon(modifier: Modifier = Modifier) {
+
+        val latState = remember { mutableStateOf("") }
+        val lonState = remember { mutableStateOf("") }
+
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        )  {
+            // Latitude Text Field
+            TextField(
+                modifier = Modifier.padding(2.dp).weight(1f),
+                singleLine = true,
+                value = latState.value,
+                onValueChange = {latState.value = it}
+            )
+            // Longitude Text Field
+            TextField(
+                modifier = Modifier.padding(2.dp).weight(1f),
+                singleLine = true,
+                value = lonState.value,
+                onValueChange = {lonState.value = it}
+            )
+            // Go Button
+            Button(
+                modifier = Modifier.padding(2.dp).weight(1f),
+                onClick = {
+                    try {
+                        val lat = latState.value.toDouble()
+                        val lon = lonState.value.toDouble()
+                        viewModel.let { it.latLon = LatLng(lat, lon) }
+                    }
+                    catch (e: Exception) {
+                        showToast(e.message.toString())
+                    }
+                }
+            ) {
+                Text("GO")
+            }
+        }
+
+    }
+
+    @Composable
+    fun ShowCheckpointsOnMap() {
+
+        // Add circle over university
+        val uniLatLon = LatLng(50.9079, -1.4077)
+        Circle(
+            center = uniLatLon,
+            radius = 100f,
+            opacity = 0f,
+            borderColor = "Red",
+            borderWidth = 3f
+        )
+
+        // Add polygon over St Mary's
+        val stMarysCoordinates = listOf(
+            LatLng(50.9063, -1.3914), LatLng(50.9063, -1.3905), LatLng(50.9053, -1.3905), LatLng(50.9053, -1.3914)
+        )
+        Polygon(
+            vertices = stMarysCoordinates,
+            opacity = 0f,
+            borderColor = "Green",
+            borderWidth = 3f
+        )
+
+        // Route to the railway station
+        val routeToRailwayCoordinates = listOf(
+            LatLng(50.9079, -1.4015), LatLng(50.9071, -1.4015), LatLng(50.9069, -1.4047), LatLng(50.9073, -1.4077), LatLng(50.9081, -1.4134)
+        )
+        Polyline(
+            points = routeToRailwayCoordinates,
+            lineWidth = 3f,
+            color = "Blue"
         )
 
     }
 
     @Composable
-    fun DisplayLatLon() {
+    fun DisplayLatLon(modifier: Modifier = Modifier) {
 
         // Get Lat and on values from view model
         val lat = remember { mutableStateOf(0.0) }
-        viewModel.latLiveData.observe(this){
-            lat.value = it
-        }
         val lon = remember { mutableStateOf(0.0) }
-        viewModel.latLiveData.observe(this){
-            lon.value = it
+        viewModel.latLonLiveData.observe(this){
+            lat.value = it.latitude
+            lon.value = it.longitude
         }
+
         Column(
-            modifier = Modifier.fillMaxSize().height(40.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Bottom
+            modifier = modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Row(
-                modifier = Modifier.fillMaxWidth().padding(4.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp),
                 horizontalArrangement = Arrangement.Start
             ) {
 
-                val latStr = "Latitude: ${lon.value}"
-                Text(latStr)
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(4.dp),
-                horizontalArrangement = Arrangement.Start
-            ) {
-                val lonStr = "Longitude: ${lon.value}"
-                Text(lonStr)
+                val latLonStr = "Latitude: ${lat.value} \t Longitude: ${lon.value}"
+                Text(latLonStr)
             }
         }
     }
@@ -167,10 +255,15 @@ class MainActivity : ComponentActivity(), LocationListener {
      * updates state variables every time location changes
      */
     override fun onLocationChanged(location: Location) {
-        viewModel.let {
-            it.latitude = location.latitude
-            it.longitude = location.longitude
+        if (liveLocation) {
+            viewModel.let {
+                it.latLon = LatLng(location.latitude, location.longitude)
+            }
         }
+    }
+
+    fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
     }
 
     @Deprecated("don't use this")
